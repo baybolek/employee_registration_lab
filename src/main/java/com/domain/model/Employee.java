b33a969bcd4c8c71d2fa719584a644de75cc395d
12816 <aybolek27@gmail.com>
2022-10-12 09:41:55 -0500

    Initial commit

---
+++ src/main/java/com/domain/model/Employee.java
@@ -0,0 +1,30 @@
+package com.domain.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthday;
+    private String email;
+    private String password;
+    private String address;
+    private String address2;
+    private String city;
+    private String state;
+    private String zipCode;
+}
